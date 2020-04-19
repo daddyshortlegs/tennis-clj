f0f337082a8ea62fb1c270d512c955ab4423ed20
andys <andysmith836@gmail.com>
2020-04-19 22:43:36 +0100

    Remove duplication and clean up

--- src/tennis/core.clj
+++ src/tennis/core.clj
@@ -2,27 +2,15 @@
     (:gen-class))
 
 (defn calc-score [points]
-      (let [scores {0 "Love"
-                    1 "Fifteen"
-                    2 "Thirty"
-                    3 "Forty"}]
-           (get scores points)
-           )
+      (get {0 "Love", 1 "Fifteen", 2 "Thirty" 3 "Forty"} points)
       )
 
 (defn deuce-or-all [p1score p1points]
-      (if (>= p1points 3)
-        "Deuce"
-        (str p1score "-All")
-        )
+      (if (>= p1points 3) "Deuce" (str p1score "-All"))
       )
 
-(defn is-win-player1? [p1points p2points]
-      (and (= 4 p1points) (not= p1points p2points))
-      )
-
-(defn is-win-player2? [p1points p2points]
-      (and (= 4 p2points) (not= p1points p2points))
+(defn is-win? [p1points p2points]
+      (and (= 4 (max p1points p2points)) (not= p1points p2points))
       )
 
 (defn winning-player [p1points p2points]
@@ -35,17 +23,13 @@
 
 (defn score [p1points p2points]
       (let [p1score (calc-score p1points)
-            p2score (calc-score p2points)
-            ]
+            p2score (calc-score p2points)]
 
            (cond
              (is-advantage? p1points p2points) (str "Advantage player " (winning-player p1points p2points))
-             (is-win-player1? p1points p2points) "Win for player 1"
-             (is-win-player2? p1points p2points) "Win for player 2"
-             (= p1score p2score) (deuce-or-all p1score p1points)
+             (is-win? p1points p2points) (str "Win for player " (winning-player p1points p2points))
+             (= p1points p2points) (deuce-or-all p1score p1points)
              :else (str p1score "-" p2score)
-
              )
-
            )
       )
